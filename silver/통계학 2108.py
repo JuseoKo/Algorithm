f9d71353eb3aaa26487bfdbe59a6fcb5bc01e6af
maria <mariat1717@daum.net>
2022-12-09 19:30:25 +0900

    fix

--- silver/통계학 2108.py
+++ silver/통계학 2108.py
@@ -17,44 +17,45 @@ print(res)
 print(num_list[round(int(len(num_list)/2), 1)])
 
 #세번째줄 최빈값(최빈값(가장 많이 관측되는 수)이 여러개일 경우 두번째 최빈값)
-#카운트, 숫자
-bin_f, bin_s, bin, p = [0, num_list[0]], [0, 0], 0, 0
-
-if len(num_list) != 1:
-    bin_f = [0, num_list[1]]
-
-for i in range(0, len(num_list)):
-    #카운트 증가
-    bin += 1
-    if num_list[i] != num_list[i-1]:
-        #최빈값이 업데이트 된다면
-        if bin_f[0] < bin:
-            #두개 이상일 경우를 대비해 따로 저장
-            bin_s = bin_f
-            #업데이트
-            bin_f = [bin, num_list[i]]
-            p, bin = 2, 0
-            # print(bin_f, bin_s)
-        #최빈값이 여러개라면
-        elif bin_f[0] == bin:
-            p, bin = 1, 0
+def clu():
+    global bin, bin_f, bin_s, f
+    # print(f'헤더 -> {num_list[i]}, 카운트 : {bin_f}, f값 {f}')
+    #카운트 센게 더 많으면 갱신
+    if bin > bin_f[0]:
+        bin_f = [bin, num_list[i]]
+        # print(f'큳 -> {num_list[i]}, 카운트 : {bin}변경점 : {bin_f}')
+        f, bin = 0, 0
+    #카운트 센거와 같으면 세컨드
+    elif bin == bin_f[0]:
+        f += 1
+        if f == 1:
+            bin_s = [bin, num_list[i]]
+        # print(f'같다 -> {num_list[i]}, 카운트 : {bin}변경점 : {bin_s}')
+        bin = 0
+    #카운트 센게 작을경우
     else:
-        #같다면 패스
-        pass
-#모두 한개씩만 존재한다면
-#최빈값이 여러개
-if p == 1:
+        bin = 0
+        #[1 1 1 2 3 3 3 4 4 4 5]
+#카운트, 숫자
+bin, bin_f, bin_s, f= 0, [0, num_list[0]], [0, 0], 0
+for i in range(0, len(num_list)):
+    bin += 1
+    # 0 < i < num_list 일경우
+    if i < len(num_list)-1:
+        #미래값과 다를경우
+        if num_list[i] != num_list[i+1]:
+            clu()
+        # 미래값과 같을경우 카운트 +1
+        else:
+            pass
+    # i = num_list일경우
+    elif i == len(num_list)-1:
+        if num_list[i] == num_list[i-1]:
+            clu()
+
+if f >= 1:
     print(bin_s[1])
-#최빈값이 한개
 else:
     print(bin_f[1])
-
-#최빈값 판별
-#1. 같은 숫자가 몇번 반복되는지 센다
-#2. 센 숫자를 저장한다
-#3. 최빈값이 업데이트 될 경우 갈아끼우고 최빈값이 두개 이상일 경우를 대비해 따로 저장해둔다
-#4. 만약 최빈값이 두개 이상이라면 두번째로 작은 수를 출력한다
-
 #네번째줄 범위
-print(num_list[-1], num_list[0])
 print(int(num_list[-1])-int(num_list[0]))
