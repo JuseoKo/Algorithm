b808208bc6bfdbc5534d82a668c37cb628da9b0b
maria <mariat1717@daum.net>
2022-12-11 19:23:50 +0900

    1427 add

---
+++ silver/소트인사이트 1427.py
@@ -0,0 +1,13 @@
+import sys
+#입력
+N = sys.stdin.readline()
+table = []
+sort = ''
+for i in range(0, len(N)):
+    table.append(N[len(N)-i-1])
+table.sort()
+
+for i in range(0, len(table)):
+    sort += table[len(table)-i-1]
+
+print(sort)
