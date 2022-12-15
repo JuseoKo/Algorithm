4adafd627ff138ebbe2a786b19e73ff9826266fa
maria <mariat1717@daum.net>
2022-12-15 11:27:50 +0900

    9093 add

---
+++ Bronze/단어뒤집기 9093.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+sr = []
+for i in range(0, n):
+    x = sys.stdin.readline()
+    sr.append(x)
+for i in range(0, len(sr)):
+    sr_s = sr[i].split(' ')
+    c = ''
+    for j in range(0, len(sr_s)):
+        if j != 0:
+            c = c+' '
+        for k in range(0, len(sr_s[j])):
+            c = c+sr_s[j][len(sr_s[j])-k-1]
+    print(c.replace('\n', ''))
