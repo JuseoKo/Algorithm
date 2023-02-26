82d1b41306b094a9c4a70c62ac8a2a51517dbe82
maria <mariat1717@daum.net>
2023-02-26 17:41:55 +0900

    add

---
+++ Sql/최대,최소값 구하기.sql
@@ -0,0 +1,15 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59415
+SELECT MAX(DATETIME)
+From ANIMAL_INS;
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59038
+SELECT MAX(DATETIME)
+From ANIMAL_INS;
+
+--https://school.programmers.co.kr/learn/courses/30/lessons/59406
+SELECT count(ANIMAL_ID) as 'count'
+From ANIMAL_INS;
+
+--https://school.programmers.co.kr/learn/courses/30/lessons/59408
+SELECT count(DISTINCT NAME) as 'count'
+From ANIMAL_INS;
