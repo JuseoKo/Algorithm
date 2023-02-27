b6c44b30f0fd4e5ba47747626fd3298aabaecea4
maria <mariat1717@daum.net>
2023-02-27 18:24:24 +0900

    추가

---
+++ silver/오큰수 17298.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+num = list(map(int, sys.stdin.readline().split()))
+res = [-1]*n
+stack = [0]
+
+for i in range(1, n):
+    while stack and num[stack[-1]] < num[i]:
+        res[stack.pop()] = num[i]
+    stack.append(i)
+
+for i in range(len(res)):
+    print(res[i], end=' ')
