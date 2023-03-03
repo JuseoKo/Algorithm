9fd85e403e3905d0c24f21be190c6b6525396bfd
maria <mariat1717@daum.net>
2023-03-03 14:08:18 +0900

    추가 sql

---
+++ silver/2×n 타일링 2  11727.py
@@ -0,0 +1,16 @@
+"""
+n = 1 , 1
+n = 2 , 3
+n = 3 , 5
+n = 4 , 11
+즉 점화식은 (n-1)+2*(n-2)
+다이나믹 프로그래밍을 이용해 이전 계산 결과를 활용하면 다음과 같음
+"""
+import sys
+
+n = int(sys.stdin.readline())
+arr = [1, 3]+[0]*1000
+for i in range(2, n):
+    arr[i] = arr[i-1]+2*arr[i-2]
+
+print(arr[n-1]%10007)
