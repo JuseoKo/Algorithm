8013e69b50b18e54463e5d589338baef51ba3767
maria <mariat1717@daum.net>
2023-03-07 23:48:20 +0900

    fix and add

--- Sql/동물의 아이디와 이름.sql
+++ Sql/동물의 아이디와 이름.sql
@@ -2,3 +2,12 @@
 SELECT  ANIMAL_ID, NAME
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID ASC
+
+-- 동물 수 찾기 https://school.programmers.co.kr/learn/courses/30/lessons/59041
+SELECT *
+FROM (SELECT  name,  COUNT(name) as COUNT
+      FROM ANIMAL_INS
+      WHERE NAME IS NOT NULL
+      GROUP BY name
+      ORDER BY name) x
+WHERE COUNT > 1
