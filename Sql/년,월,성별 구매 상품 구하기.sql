1ce8d84e87d19c939349da7c52a6f2bcfb581ab9
maria <mariat1717@daum.net>
2023-03-09 09:14:15 +0900

    add sql

--- Sql/년,월,성별 구매 상품 구하기.sql
+++ Sql/년,월,성별 구매 상품 구하기.sql
@@ -3,3 +3,10 @@ SELECT YEAR(SALES_DATE) as YEAR, MONTH(SALES_DATE) as MONTH, USER_INFO.GENDER, C
 from USER_INFO, ONLINE_SALE
 where USER_INFO.USER_ID = ONLINE_SALE.USER_ID and not GENDER is null
 group by YEAR, MONTH, GENDER
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59412
+SELECT HOUR(DATETIME) as HOUR, count(*) as COUNT
+from ANIMAL_OUTS
+group by HOUR
+having HOUR > 8 and HOUR < 20
+order by HOUR
