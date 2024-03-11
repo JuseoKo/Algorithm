8e5b31b6a9215e1cad81d8e2e4160b0d6d6f55da
JuseoKo <mariat1717@daum.net>
2024-03-11 23:07:29 +0900

    add: 탑 2493

---
+++ gold/탑 2493.py
@@ -0,0 +1,26 @@
+# 입력 : 탑의 수 N, 각 탑의 높이
+# 출력 : 각 탑의 수신 탑의 번호
+
+import sys
+
+N = sys.stdin.readline()
+top = list(map(int, input().split()))
+# 첫번째 탑이 보낸 신호는 누구도 받을 수 없으므로 무조건 0이다.
+result = [0]
+stack = []
+
+for i in range(int(N)):
+    while stack:
+        if stack[-1][1] > int(top[i]):
+            result.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+            if len(stack) == 0:
+                result.append(0)
+                break
+
+    stack.append([i, int(top[i])])
+
+
+print(' '.join(map(str, result)))
